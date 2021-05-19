e57fbf312fc41cedadc036cd7627213fc5b7d0e3
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2021-05-19 17:02:49 +0100

    removed intital div in table page

--- src/pages/entry-page/table-view-page/table-view-page.component.jsx
+++ src/pages/entry-page/table-view-page/table-view-page.component.jsx
@@ -9,9 +9,6 @@ import FloatingAddButton from "../../../components/floating-add-button/floating-
 import Table from "../../../components/tables/table/table.component";
 
 import { Container } from "../../../components/styles/container/container.styles";
-import { Div } from "../../../components/styles/div/div.styles";
-import { Heading } from "../../../components/styles/heading/heading.styles";
-import { Span } from "../../../components/styles/span/span.styles";
 
 const TableViewPage = ({ currentUser, showFloatingAddButton }) => {
   return (
@@ -20,14 +17,6 @@ const TableViewPage = ({ currentUser, showFloatingAddButton }) => {
         <Link to="/add-entry">
           <FloatingAddButton />
         </Link>
-      ) : null}
-
-      {showFloatingAddButton === true ? (
-        <Div>
-          <Heading style={{ marginBottom: "-20px" }}>Shopping List</Heading>
-          <br />
-          <Span>&#128722;</Span>
-        </Div>
       ) : null}
 
       <div>
